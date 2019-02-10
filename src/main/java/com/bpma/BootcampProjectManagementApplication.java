a6be3c97208253ea5d0b39a4814e89757389aea8
Sean Blessing <snblessing@gmail.com>
2019-02-10 11:20:45 -0500

    initial commit

---
+++ src/main/java/com/bpma/BootcampProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package com.bpma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampProjectManagementApplication.class, args);
+	}
+
+}
+
